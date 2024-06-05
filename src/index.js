f11d9be158a33f339129609183617df2ccf4317a
Cade Larrabee <larrabeecade@gmail.com>
2024-06-05 13:31:21 -0400

    Setting up jest, and fixing template for use

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-import "./style.css";
-import { Project } from "./projectModule.js";
-import { Task } from "./taskModule.js";
-import { DomController } from "./DomManip.js";
-import { format } from "date-fns";
-
-function OnEntry() {
-  const DomManip = new DomController();
-
-  DomManip.GenerateOnEntry();
-}
+function OnEntry() {}
 
 OnEntry();
+//
